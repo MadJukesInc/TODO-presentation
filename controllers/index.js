3cb24dafc5af16690c9cb293ab94ce44da266a3f
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2015-09-09 12:59:52 -0300

    feat: API endpoints

--- controllers/index.js
+++ controllers/index.js
@@ -1,16 +1,61 @@
 'use strict';
 
-var IndexModel = require('../models/index');
+var TodoModel = require('../models/Todo');
 
+module.exports = function(router) {
+    router.get('/todos', function(req, res) {
+        var query = req.query;
 
-module.exports = function (router) {
+        TodoModel.find(query, function(err, results) {
+            if (err) { return res.status(500).send(err); }
 
-    var model = new IndexModel();
-
-    router.get('/', function (req, res) {
-        
-        res.send('<code><pre>' + JSON.stringify(model, null, 2) + '</pre></code>');
-        
+            res.status(200).send(results);
+        });
     });
 
+    router.get('/todos/:id', function(req, res) {
+        var TodoID = req.params.id;
+
+        TodoModel.findById(TodoID, function(err, found) {
+            if (err) { return res.status(500).send(err); }
+
+            res.status(200).send(found);
+        });
+    });
+
+    router.post('/todos', function(req, res) {
+        var newTodo = req.body;
+
+        TodoModel.create(newTodo, function(err, created) {
+            if (err) { return res.status(500).send(err); }
+
+            res.status(200).send(created);
+        });
+    });
+
+    router.put('/todos/:id', function(req, res) {
+        var TodoID = req.params.id;
+        var updatedTodo = req.body;
+
+        TodoModel.findByIdAndUpdate(TodoID, { $set: updatedTodo },
+            function(err, found) {
+                if (err) { return res.status(500).send(err); }
+
+                TodoModel.findById(TodoID, function(err, updated) {
+                    if (err) { return res.status(500).send(err); }
+
+                    res.status(200).send(updated);
+                });
+            });
+    });
+
+    router.delete('/todos/:id', function(req, res) {
+        var TodoID = req.params.id;
+
+        TodoModel.findByIdAndRemove(TodoID, function(err, found) {
+            if (err) { return res.status(500).send(err); }
+
+            res.status(200).send(found);
+        });
+    });
 };
